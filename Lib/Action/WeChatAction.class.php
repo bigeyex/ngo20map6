6ef5e5be6fdc04cabd74d07f0575aadc2e33f919
Wang Yu <wangyu@WangmatoMacBook-Pro.local>
2013-12-30 23:20:11 -0500

    add wechat endpoint

---
+++ Lib/Action/WeChatAction.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class WechatAction extends Action {
+
+	function endpoint(){
+		$signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+}
